4165fa2eace0036d682692727435a4dfab4c5536
Rochmady <srochmady@gmail.com>
2026-06-24 07:10:31 +0700

    Update composer installed.php reference
    Bump the git reference for sangia/sciecola in vendor/composer/installed.php from 901c9284312fc2386226c5195fe4bcb9b8edab83 to fbda0dcae0d69688ff467a511e48e852884da731. This reflects an updated commit after dependency update/installation.

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'sangia/sciecola',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '901c9284312fc2386226c5195fe4bcb9b8edab83',
+        'reference' => 'fbda0dcae0d69688ff467a511e48e852884da731',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         'sangia/sciecola' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '901c9284312fc2386226c5195fe4bcb9b8edab83',
+            'reference' => 'fbda0dcae0d69688ff467a511e48e852884da731',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
