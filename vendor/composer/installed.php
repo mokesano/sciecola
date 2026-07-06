d0a5641451b0282c592f724fb09de48b069a475d
Rochmady <srochmady@gmail.com>
2026-07-07 00:55:35 +0700

    Update project references and add new SVG icons
    - Updated the reference for the sangia/sciecola package in installed.php.
    - Added new SVG icons: Demonstrate.svg, Discover.svg, and Find.svg to the assets/icon directory.

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'sangia/sciecola',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'fbda0dcae0d69688ff467a511e48e852884da731',
+        'reference' => '4c72b2bfc875143c180f3f31e7517cbb688b2956',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         'sangia/sciecola' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'fbda0dcae0d69688ff467a511e48e852884da731',
+            'reference' => '4c72b2bfc875143c180f3f31e7517cbb688b2956',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
