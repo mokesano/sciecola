f73cd10f71630c281c8f0dee4e168a61826665f5
Rochmady <srochmady@gmail.com>
2026-07-26 16:32:11 +0700

    refactor: replace react-router-dom with react-router for consistency

--- frontend-src/src/components/sdg/SdgTrendChart.jsx
+++ frontend-src/src/components/sdg/SdgTrendChart.jsx
@@ -1,5 +1,5 @@
 import React, { useMemo } from 'react';
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router';
 import { useTranslation } from 'react-i18next';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 import { SDG_DICTIONARY } from '../../utils/sdgDataAdapter';
